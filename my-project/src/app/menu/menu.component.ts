6a962e39477e3ce8a8e66295f64e394e36e7badc
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-23 23:34:41 +0200

    add style in menu component

--- my-project/src/app/menu/menu.component.ts
+++ my-project/src/app/menu/menu.component.ts
@@ -22,24 +22,30 @@ export class MenuComponent implements OnInit{
     };
     this.slides[1] = {
       id: 1,
+      src: '../../assets/img/slider/2.jpg',
+      title: 'Second slide',
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    }
+    this.slides[2] = {
+      id: 2,
       src: '../../assets/img/slider/3.jpg',
       title: 'Third slide',
       subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
     },
-    this.slides[2] = {
-      id: 2,
+    this.slides[3] = {
+      id: 3,
       src: '../../assets/img/slider/4.jpg',
       title: 'Fourth slide',
       subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
     },
-    this.slides[3] = {
-      id: 3,
+    this.slides[4] = {
+      id: 4,
       src: '../../assets/img/slider/5.jpg',
       title: 'Five slide',
       subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
     },
-    this.slides[4] = {
-      id: 4,
+    this.slides[5] = {
+      id: 5,
       src: '../../assets/img/slider/6.jpg',
       title: 'Six slide',
       subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
