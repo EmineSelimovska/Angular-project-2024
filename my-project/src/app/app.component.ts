2fed0c3e818255650413b1756ea2f05fe3e31700
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-02-23 23:22:28 +0200

    add promo-banner components

--- my-project/src/app/app.component.ts
+++ my-project/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MenuAreaComponent } from './menu-area/menu-area.component';
 import { MenuComponent } from './menu/menu.component';
+import { PromoBannerComponent } from './promo-banner/promo-banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,
-   MenuAreaComponent,MenuComponent],
+  imports: [RouterOutlet, HeaderComponent,
+    MenuAreaComponent, MenuComponent, PromoBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
