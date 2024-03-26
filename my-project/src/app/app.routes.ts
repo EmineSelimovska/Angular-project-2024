8bf7ba92e06d032afcd1ca13d52c352f7e73d022
EmineSelimovska <114228146+EmineSelimovska@users.noreply.github.com>
2024-03-26 13:45:56 +0200

    add Register components

--- my-project/src/app/app.routes.ts
+++ my-project/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HeaderComponent } from './core/header/header.component';
 import { PropertiesComponent } from './properties/properties.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {path: 'header', component: HeaderComponent},
     {path: 'properties', component: PropertiesComponent},
     {path: '', component: HeaderComponent },
     {path: 'contact', component: ContactComponent},
-     {path: 'login', component: LoginComponent}
+     {path: 'login', component: LoginComponent},
+     {path: 'register', component: RegisterComponent}
 ];
 
